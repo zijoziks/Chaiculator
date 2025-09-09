f4fd9ed767d16dd8fdfbb81ebcc59c0776ae90bc
zijoziks <zijadcajdin@gmail.com>
2025-09-09 23:43:38 +0200

    Trying out git

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use std::io;
+
+fn output(expression: &mut str){
+    println!("Expression: {}", expression);
+}
 fn main() {
-    println!("Hello, world!");
+    println!("Enter desired expression: ");
+    let mut expression = String::new();
+    io::stdin().read_line(&mut expression).expect("Failed to read line");
+    output(&mut expression);
 }
