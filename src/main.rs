28cb37e806156e5221b4ff96ad634c3fdf6c2152
zijoziks <zijadcajdin@gmail.com>
2025-09-12 17:11:41 +0200

    very basic token parser

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,66 @@
 use std::io;
+use std::vec::Vec;
 
 // u8 represent priority value
 // the higher the number is, higher the priority
 enum Token {
     Number(i32),
-    Plus(char, u8),
-    Minus(char, u8),
-    Multiply(char, u8),
-    Divide(char, u8)
+    Plus(u8),
+    Minus(u8),
+    Multiply(u8),
+    Divide(u8),
+    Invalid
+}
+
+// TODO replace magic variables with global constants
+fn deduct_token(deduct_from: char) -> Token{
+    match deduct_from {
+        '+' => Token::Plus(1),
+        '-' => Token::Minus(1),
+        '*' => Token::Multiply(2),
+        '/' => Token::Divide(2),
+        _ => {
+            println!("Huh?");
+            Token::Invalid
+        }
+    }
+}
+
+// TODO finish this function, last thing I did was parse a number, also need to do testing
+fn convert_to_tokens(expression: &str) -> Vec<Token> {
+    let mut tokens: Vec<Token> = Vec::new();
+
+    let mut first_index: Option<usize> = None;
+
+    for (i, c) in expression.chars().enumerate() {
+
+        // Encounter first digit
+        if c.is_ascii_digit() && first_index.is_none() {
+            first_index = Some(i);
+        }
+
+        // Encounter an operator
+        if !c.is_ascii_digit() && !first_index.is_none() {
+            if let Some(index) = first_index {
+
+                let num: i32 = expression[index..i].parse().expect("Couldn't parse number");
+                tokens.push(Token::Number(num));
+
+                if c == '\n' {
+                    break;
+                }
+
+                let token = deduct_token(c);
+                // TODO error handling for invalid cases and invalid token vector
+                // TODO handles '/n'
+                tokens.push(token);
+            }
+
+            first_index = None;
+        }
+
+    }
+    tokens
 }
 
 fn output(expression: &mut str){
@@ -17,5 +70,6 @@ fn main() {
     println!("Enter desired expression: ");
     let mut expression = String::new();
     io::stdin().read_line(&mut expression).expect("Failed to read line");
+    let tokens: Vec<Token> = convert_to_tokens(&expression);
     output(&mut expression);
 }
