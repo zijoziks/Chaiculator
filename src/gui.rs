12234e5554b442e636dba985c84ad576cf4c9f69
zijoziks <zijadcajdin@gmail.com>
2025-10-21 09:51:01 +0200

    Updated dependencies and a little work on UI

--- src/gui.rs
+++ src/gui.rs
@@ -5,5 +5,8 @@ slint::include_modules!();
 pub fn gui() -> Result<(), slint::PlatformError> {
     let main_window = MainWindow::new()?;
 
+    let ui_handle = main_window.as_weak();
+
+
     main_window.run()
 }
